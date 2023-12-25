83e33c7b6d15f9647f054942e7959398a58d9ba2
Frowmza <66181451+Frowmza@users.noreply.github.com>
2023-12-25 15:31:17 +0100

    last build

--- web/src/components/Icon.tsx
+++
@@ -1,8 +0,0 @@
-import {ReactNode} from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { IconProp, SizeProp } from '@fortawesome/fontawesome-svg-core';
-
-export const Icon = (icon:IconProp, size?: SizeProp, beat?: boolean): ReactNode => {
-    return <FontAwesomeIcon icon={icon} size={size || "2xl"} beat={beat} className='card-icon' />
-}
-

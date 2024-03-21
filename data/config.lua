b1e8ed6fff8f9839d00eb9cc436bbe29128e7700
yiruzu <94981325+yiruzu@users.noreply.github.com>
2024-03-21 18:06:20 +0100

    Added Scale Option to Config

--- data/config.lua
+++ data/config.lua
@@ -3,7 +3,7 @@ return {
     locations = {
         {
             pos = vector4(727.74, -1088.95, 22.17, 90.0),
-            blip = { sprite = 72, color = 1, shortRange = true, label = 'Customs' },
+            blip = { sprite = 72, scale = 1.0, color = 27, shortRange = true, label = 'Customs' },
             mods = {'decals', 'paint'},
             free = true,
             -- group = {
@@ -11,12 +11,12 @@ return {
             -- },
         },
 
-        { pos = vector4(-206.109, -1328.886, 30.67899, 90.0), blip = { sprite = 72, color = 1, shortRange = true, label = 'Customs' }, free = true },
-        { pos = vector4(868.22, -1350.14, 25.97, 90.0),       blip = { sprite = 72, color = 1, shortRange = true, label = 'Customs' }, free = true },
-        { pos = vector4(450.25, -975.71, 25.7, 90.0),         blip = { sprite = 72, color = 1, shortRange = true, label = 'Customs' }, free = true },
-        { pos = vector4(110.8, 6626.46, 31.89, 90.0),         blip = { sprite = 72, color = 1, shortRange = true, label = 'Customs' }, free = true },
-        { pos = vector4(1695.14, 3588.98, 35.29, 90.0),       blip = { sprite = 72, color = 1, shortRange = true, label = 'Customs' }, free = true },
-        { pos = vector4(-1269.103, -3376.59, 13.94014, 90.0), blip = { sprite = 72, color = 1, shortRange = true, label = 'Customs' }, free = true },
+        { pos = vector4(-206.109, -1328.886, 30.67899, 90.0), blip = { sprite = 72, scale = 1.0, color = 27, shortRange = true, label = 'Customs' }, free = true },
+        { pos = vector4(868.22, -1350.14, 25.97, 90.0),       blip = { sprite = 72, scale = 1.0, color = 27, shortRange = true, label = 'Customs' }, free = true },
+        { pos = vector4(450.25, -975.71, 25.7, 90.0),         blip = { sprite = 72, scale = 1.0, color = 27, shortRange = true, label = 'Customs' }, free = true },
+        { pos = vector4(110.8, 6626.46, 31.89, 90.0),         blip = { sprite = 72, scale = 1.0, color = 27, shortRange = true, label = 'Customs' }, free = true },
+        { pos = vector4(1695.14, 3588.98, 35.29, 90.0),       blip = { sprite = 72, scale = 1.0, color = 27, shortRange = true, label = 'Customs' }, free = true },
+        { pos = vector4(-1269.103, -3376.59, 13.94014, 90.0), blip = { sprite = 72, scale = 1.0, color = 27, shortRange = true, label = 'Customs' }, free = true },
     },
     -- highest price | means the last mod of spoiler (for ex) gonna cost $2000 / if you want to make the price higher or lower just increase price then all spoiler mods gonna price increase
 }

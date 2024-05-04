f8766b650a0eb5e4e596d5c9af9c16c08e8640ac
Scully <51968381+Scullyy@users.noreply.github.com>
2024-05-04 04:17:58 -0700

    Add horn labels so they don't show as NULL

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,41 +1,36 @@
-fx_version "cerulean"
+fx_version 'cerulean'
 use_experimental_fxv2_oal 'yes'
 lua54 'yes'
 game 'gta5'
 
-author "Byte Labs"
+author 'Byte Labs'
 version '2.0.1'
-
 repository 'Byte-Labs-Project/bl_customs'
-
-ui_page 'web/build/index.html'
---ui_page 'http://localhost:5173/'
 
 dependency 'bl_bridge'
 
-client_scripts {
-  "@bl_bridge/imports/client.lua",
-  "client/init.lua"
-}
+shared_script '@ox_lib/init.lua'
 
-dependencies {
-  'bl_bridge',
+client_scripts {
+  '@bl_bridge/imports/client.lua',
+  'client/init.lua'
 }
 
 server_scripts {
-  "@bl_bridge/imports/server.lua",
-  "server/main.lua"
+  '@bl_bridge/imports/server.lua',
+  'server/main.lua'
 }
 
-shared_script '@ox_lib/init.lua'
+ui_page 'web/build/index.html'
+--ui_page 'http://localhost:5173/'
 
 files {
   'data/*.lua',
   'client/modules/*.lua',
   'web/build/index.html',
   'web/build/**/*',
-  "data/chameleon/*.meta",
+  'data/chameleon/*.meta',
 }
 
-data_file "CARCOLS_GEN9_FILE" "data/chameleon/carcols_gen9.meta"
-data_file "CARMODCOLS_GEN9_FILE" "data/chameleon/carmodcols_gen9.meta"
+data_file 'CARCOLS_GEN9_FILE' 'data/chameleon/carcols_gen9.meta'
+data_file 'CARMODCOLS_GEN9_FILE' 'data/chameleon/carmodcols_gen9.meta'
